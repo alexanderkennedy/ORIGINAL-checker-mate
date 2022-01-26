cdd13a90f6433d1d3dc4bf64d14e0a405aa6ccc8
Alex Kennedy <alexdkennedy@gmail.com>
2022-01-26 15:27:21 -0800

    login form styled

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import reactRouterDom, { Link } from 'react-router-dom';
-
+import "./Login.scss";
 
 
 class Login extends Component {
     render() {
         return (
-            <div>
-                <input type="text" placeholder="User Name" />
-                <input type="password" placeholder="Password" />
+            <div className="Login__wrap">
+                <input className='Login__name' type="text" placeholder="User Name" />
+                <input className='Login__password' type="password" placeholder="Password" />
               <Link to="/MainList">
                 <button>Login</button>
                 <button>Forgot Password?</button>
